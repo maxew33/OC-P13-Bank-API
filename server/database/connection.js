3a5c97b624a97bb855dfbc972c0cb8abd0fbef3c
Maxime Malfilatre <maxime.malfilatre@gmail.com>
2023-06-30 16:35:58 +0200

    :bug: [mongo] change mongo adress and link in readme

--- server/database/connection.js
+++ server/database/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 const databaseUrl =
-  process.env.DATABASE_URL || 'mongodb://localhost/argentBankDB'
+  process.env.DATABASE_URL || 'mongodb://0.0.0.0/argentBankDB'
 
 module.exports = async () => {
   try {
